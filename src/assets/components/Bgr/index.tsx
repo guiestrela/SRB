e70f922b54bd0807072a77f29cecc28e50bb91d4
Guilherme Estrela <guilhermestrela@gmail.com>
2025-05-26 14:07:50 -0300

    fix(Bgr): Correct API endpoint and add headers
    - Fixed incorrect API endpoint URL.
    - Added API key header for authentication.

--- src/assets/components/Bgr/index.tsx
+++ src/assets/components/Bgr/index.tsx
@@ -18,7 +18,7 @@ function Bgr() {
     const [result, setResult] = useState<string | null>(null);
     const [loading, setLoading] = useState(false);
 
-    //const API_KEY = "68H8bJoimFogKoJ9667rcy9R"; // Coloque sua chave aqui
+    const API_KEY = "68H8bJoimFogKoJ9667rcy9R"; // Coloque sua chave aqui
 
     const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -36,8 +36,11 @@ function Bgr() {
     formData.append("size", "auto");
 
     try {
-        const response = await fetch("/api/removebg", {
+        const response = await fetch("https://api.remove.bg/v1.0/removebg", {
             method: "POST",
+            headers: {
+                "X-Api-Key": API_KEY,
+            },
             body: formData,
         });
 
